0030821c7b9cb2ca980476ae3b3f01e24e222f70
muhammadadamfirdaus <adam@muhammadadamfirdaus.com>
2024-03-24 09:22:07 +0700

    fix some header view on ipad

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -6,26 +6,36 @@ import { HiMagnifyingGlass } from "react-icons/hi2";
 export default function Header() {
   return (
     <div className="fixed top-0 left-0 z-10 w-full h-[57px] lg:h-[100px]">
-      <div className="flex items-center h-full lg:px-32">
-        <div className="px-4 lg:order-1">
+      <div className="flex items-center h-full md:px-20 2xl:px-32">
+        <div className="px-4 md:order-1">
           <Menu />
         </div>
 
-        <div className="flex justify-center w-full lg:w-auto">
+        <div className="flex justify-center w-full md:w-auto">
           <Image src="/images/logo/logo.png" alt="Logo" width={115} height={40} priority />
         </div>
 
-        <div className="px-4 lg:order-2">
+        <div className="px-4 md:order-2">
           <div className="lg:flex lg:py-3 lg:pl-4 lg:pr-8 lg:bg-white lg:rounded-3xl">
             <HiMagnifyingGlass className={`mr-2`} size={20} />
-            <input type="text" name="search" id="search" placeholder="Search something here!" className="border-0 outline-none hidden lg:block text-sm" />
+            <input type="text" name="search" id="search" placeholder="Search something here!" className="border-0 outline-none hidden lg:flex text-sm" />
           </div>
         </div>
 
-        <div className="lg:order-3 hidden md:block">
-          <button className=" bg-blue-950 xl:px-4 2xl:px-7 py-3 rounded-3xl lg:text-xs 2xl:text-sm">
+        <div className="hidden xl:flex justify-center flex:flex-auto md:order-3">
+          <button className=" bg-blue-950 lg:px-4 2xl:px-7 py-3 rounded-3xl lg:text-xs 2xl:text-sm">
             <span className="text-white">Join the community</span>
           </button>
+        </div>
+
+        <div className="hidden md:flex justify-end md:order-4 md:flex-auto">
+          <div className="flex md:px-1 lg:px-6">
+            <Image src="/icons/flag-vietnam.svg" width={16} height={16} alt="flag Vietnam" />
+            <select id="countries" className="bg-transparent text-gray-900 text-sm rounded-lg focus:ring-zinc-400 focus:border-zinc-400 w-full py-4 basis-full">
+              <option defaultValue={`VND`}>VND</option>
+              <option value="USD">USD</option>
+            </select>
+          </div>
         </div>
       </div>
     </div>
--- app/components/Menu.jsx
+++ app/components/Menu.jsx
@@ -10,30 +10,26 @@ export default function Menu() {
 
   return (
     <>
-      <div className="flex justify-end lg:hidden">
+      <div className="flex justify-end md:hidden">
         <HiOutlineBars3 onClick={() => setToggleMenu(!toggleMenu)} className={`${toggleMenu ? "hidden" : "block"}`} size={24} />
       </div>
-      <div className={`bg-slate-900 text-white fixed top-0 left-0 w-full h-full lg:relative lg:h-auto lg:w-1/5 ${toggleMenu ? "block" : "hidden"} lg:block lg:bg-transparent lg:text-black`}>
-        <ul className="lg:flex w-full py-4 h-full md:py-0">
+      <div className={`bg-slate-900 text-white fixed top-0 left-0 w-full h-full md:relative md:h-auto md:w-1/5 ${toggleMenu ? "block" : "hidden"} md:block md:bg-transparent md:text-black`}>
+        <ul className="md:flex w-full py-4 h-full md:py-0 z-50">
           <li className="flex px-4 justify-end md:block">
             <span className="flex items-center py-4 basis-8 md:hidden" onClick={() => setToggleMenu(!toggleMenu)}>
               <HiXMark />
             </span>
           </li>
-          <li className="flex px-4">
-            <span className="flex items-center py-4 basis-8">{/* <HiOutlineDeviceTablet /> */}</span>
+          <li className="flex px-4 mb-4 lg:mb-0">
             <span className="flex items-center ml-2">Home</span>
           </li>
-          <li className="flex px-4">
-            <span className="flex items-center py-4 basis-8">{/* <HiOutlineDeviceTablet /> */}</span>
+          <li className="flex px-4 mb-4 lg:mb-0">
             <span className="flex items-center ml-2">Category</span>
           </li>
-          <li className="flex px-4">
-            <span className="flex items-center py-4 basis-8">{/* <HiOutlineDeviceTablet /> */}</span>
+          <li className="flex px-4 mb-4 lg:mb-0">
             <span className="flex items-center ml-2">About</span>
           </li>
-          <li className="flex px-4">
-            <span className="flex items-center py-4 basis-8">{/* <HiOutlineDeviceTablet /> */}</span>
+          <li className="flex px-4 mb-4 lg:mb-0">
             <span className="flex items-center ml-2">Contact</span>
           </li>
         </ul>

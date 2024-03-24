567ddbe47ede282525d2a5205e38d3d30af31f22
muhammadadamfirdaus <adam@muhammadadamfirdaus.com>
2024-03-24 08:16:51 +0700

    test deploy

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
     <footer>
       <div className="pt-16 pb-8 px-4 md:px-8 lg:px-32">
         <div className="lg:flex bg-[#003459] p-8">
-          <h3 className="font-bold text-2xl text-white mb-6 text-center lg:mb-0 lg:text-left lg:basis-2/5">Register now so you don't miss our programs</h3>
+          <h3 className="font-bold text-2xl text-white mb-6 text-center lg:mb-0 lg:text-left lg:basis-2/5">Register now so you don&apos;t miss our programs</h3>
           <div className="flex flex-col bg-white p-3 h-full rounded-lg lg:flex-row lg:ml-5 lg:basis-3/5">
             <input type="email" name="emailSubscription" id="emailSubscriptionID" placeholder="Enter your email" className="border border-slate-500 border-solid outline-none rounded-lg px-4 py-2 basis-full" />
             <button className="bg-[#003459] text-white rounded-lg px-5 mt-2 basis-full pt-[14px] pb-[10px] h-12 lg:mt-0 lg:ml-2 lg:basis-2/5 2xl:px-7">Subscribe Now</button>
